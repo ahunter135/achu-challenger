a5e76d2327eadd04ac3b26ff838d63cb825bdb00
Matt <barbuzzi.m@gmail.com>
2020-10-22 13:10:16 -0400

    PW Reset

--- src/app/password-reset/password-reset.component.ts
+++ src/app/password-reset/password-reset.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../services/http.service';
+import { ToastController, ModalController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-password-reset',
@@ -6,11 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./password-reset.component.scss'],
 })
 export class PasswordResetComponent {
+  emailreset;
+  constructor(public router: Router,public http: HttpService,public toast: ToastController, public modal: ModalController) { }
 
-  constructor() { }
 
+  async sendPwReset() {
+    console.log(this.emailreset);
+    this.http.post("/auth/Account/ForgotPassword", { email: this.emailreset }).then(async () => {
+      let t = await this.toast.create({
+        message: 'Email Sent!',
+        duration: 2000
+      });
+      t.present();
+       
+    });
+  }
 
-  
+  async goToLogin() {
+
+    this.router.navigateByUrl("/login");
+  }
+
    
 
 }
